8ea71452e993620bfa8dc2ec8ffc992a660f0686
Deepak <bhandarideepakdev@gmail.com>
2024-07-24 04:54:28 +0300

    dirname ejs

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ import mysql from "mysql2/promise";
 import cors from "cors";
 import axios from "axios";
 import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
 
 const app = express();
 app.use(express.json());
